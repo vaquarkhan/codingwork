b7b34d8b567c46f200db2280aab4a71303f68f48
Vaquar Khan <vaquar.cna@gmail.com>
2017-01-23 10:08:33 -0600

    commit

--- RestPocProject/src/main/java/org/viquar/khan/messenger/exception/GenericExceptionMapper.java
+++ RestPocProject/src/main/java/org/viquar/khan/messenger/exception/GenericExceptionMapper.java
@@ -4,7 +4,7 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
 
-import org.koushik.javabrains.messenger.model.ErrorMessage;
+import org.viquar.khan.messenger.model.ErrorMessage;
 
 // This class intentionally doesn't have the @Provider annotation.
 // It has been disabled in order to try out other ways of throwing exceptions in JAX-RS
@@ -14,7 +14,7 @@ public class GenericExceptionMapper implements ExceptionMapper<Throwable> {
 
 	@Override
 	public Response toResponse(Throwable ex) {
-		ErrorMessage errorMessage = new ErrorMessage(ex.getMessage(), 500, "http://javabrains.koushik.org");
+		ErrorMessage errorMessage = new ErrorMessage(ex.getMessage(), 500, "http://viquar.khan.org");
 		return Response.status(Status.INTERNAL_SERVER_ERROR)
 				.entity(errorMessage)
 				.build();
