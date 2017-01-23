4ed94ee74594c98da477655417911be7bb16f177
Vaquar Khan <vaquar.cna@gmail.com>
2017-01-23 09:53:43 -0600

    commit

--- StudentEnrollmentWithREST-1.1/src/main/java/com/github/viquar/repository/StudentRepository.java
+++ StudentEnrollmentWithREST-1.1/src/main/java/com/github/viquar/repository/StudentRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.github.elizabetht.model.Student;
+import com.github.viquar.model.Student;
 
 @Repository("studentRepository")
 public interface StudentRepository extends JpaRepository<Student, Long> {
